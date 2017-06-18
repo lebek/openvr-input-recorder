beb04f2e3df751d3cf6e7cd5beeeab0a22e4277d
Peter Le Bek <peteralebek@gmail.com>
2017-06-17 20:55:39 -0400

    init

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+// A simple program that computes the square root of a number
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (int argc, char *argv[])
+{
+  if (argc < 2)
+    {
+    fprintf(stdout,"Usage: %s number\n",argv[0]);
+    return 1;
+    }
+  double inputValue = atof(argv[1]);
+  double outputValue = sqrt(inputValue);
+  fprintf(stdout,"The square root of %g is %g\n",
+          inputValue, outputValue);
+  return 0;
+}
